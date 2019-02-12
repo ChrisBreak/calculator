c426c730b526e03279dcba5be72293895b3c5793
ChrisBreak <37880528+ChrisBreak@users.noreply.github.com>
2019-02-11 23:46:53 -0700

    Add files via upload

---
+++ calcCode.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $(".butt").click(function() {
+    if ($(this).html() == "C") {
+      $("#inExp").html("_");
+    }
+    else if (($(this).html() == "del")) {
+      if ($("#inExp").html().length == 1) {
+        $("#inExp").html("_");
+      }
+      else {
+        $("#inExp").html($("#inExp").html().slice(0, -1));
+      }
+    }
+    else if ($(this).html() == "=") {
+      try {
+        let result = eval($("#inExp").html());
+        $("#result").html($("#inExp").html() + " =");
+        $("#inExp").html(result);
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+            $("#result").html($("#inExp").html() + " =ERROR");
+            $("#inExp").html("_");
+        }
+      }
+    }
+    else {
+      if ($("#inExp").html() == "_") {
+        $("#inExp").html($(this).html());
+      }
+      else {
+        $("#inExp").append($(this).html());
+      }
+    }
+  });
+});
